d3aee9ffef4e460226329466e72ac9f95b1a2478
BLew10 <112529092+BLew10@users.noreply.github.com>
2022-11-25 13:45:00 -0800

    Update seeder.js

--- seeder.js
+++ seeder.js
@@ -6,6 +6,10 @@ mongoose.connect("mongodb://localhost/nfl2022schedule", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
+
+
 
 const seedWeeks = [
     {
