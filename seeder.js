b78c0f13f52b5d09194ad0bf3229acb149bbd735
BLew10 <Brandonlewis.10@gmail.com>
2023-01-03 09:38:21 -0800

    text -> password type

--- seeder.js
+++ seeder.js
@@ -6,6 +6,10 @@ mongoose.connect("mongodb://localhost/nfl2022schedule", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
+    .then(() => console.log("Established a connection to the database"))
+    .catch(err => console.log("Something went wrong when connecting to the database", err));
+
+
 
 const seedWeeks = [
     {
@@ -17603,10 +17607,10 @@ const seedWeeks = [
         "__v": 0
     }
 ]
-const seedDB = async ()=> {
-await Week.deleteMany({})
-await Week.insertMany(seedWeeks)
+const seedDB = async () => {
+    await Week.deleteMany({})
+    await Week.insertMany(seedWeeks)
 }
-seedDB().then(()=>{
+seedDB().then(() => {
     mongoose.connection.close()
 })
